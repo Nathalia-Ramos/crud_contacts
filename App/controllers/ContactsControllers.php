80a2e6276b3b08105087d403cee6ab5eeaa2aee3
nathalia <nathalia@gmail.com>
2023-12-15 21:04:50 -0300

    feat: terminando valiadações de cadastro de contato

--- App/controllers/ContactsControllers.php
+++ App/controllers/ContactsControllers.php
@@ -13,18 +13,111 @@ final class ContactsControllers  {
         try {
             $data = $request->getParsedBody();
 
+            //verifica se os campos full_name, mail existe e não estão vazios
+            if(!isset($data['full_name']) || empty($data['full_name'])) {
+                $response
+                    ->withStatus(400)
+                    ->withHeader('Content-Type', 'application/json')
+                    ->getBody()
+                    ->write(json_encode(['error' => 'O campos full_name é obrigatório e não pode ser vazios.']));
+
+                return $response;
+            }
+
+            if(!isset($data['mail']) || empty($data['mail'])) {
+                $response
+                    ->withStatus(400)
+                    ->withHeader('Content-Type', 'application/json')
+                    ->getBody()
+                    ->write(json_encode(['error' => 'O campos mail é obrigatório e não pode ser vazios.']));
+
+                return $response;
+            }
+
+            if(!isset($data['phone']) || !isset($data['cellphone'])) {
+                $response
+                    ->withStatus(400)
+                    ->withHeader('Content-Type', 'application/json')
+                    ->getBody()
+                    ->write(json_encode(['error' => 'Informar ao menos um telefone para contato.']));
+
+                return $response;
+            }
+
             $contact = new Contacts();
+
+            //verifica se existe o mail, phone ou telefone cadastrado
+            if(isset($data['mail'])){
+                $mailExistVerify = $contact->getContactByMail($data['mail']);
+                if($mailExistVerify) {
+                    $response
+                    ->withStatus(409)
+                    ->withHeader('Content-Type', 'application/json')
+                    ->getBody()
+                    ->write(json_encode(['message' => 'O email existe em nosso sistema.']));
+
+                    return  $response;
+                }
+            }
+
+            if(isset($data['phone'])){
+                if (strlen($data['phone']) > 10) {
+                    $response
+                        ->withStatus(400)
+                        ->withHeader('Content-Type', 'application/json')
+                        ->getBody()
+                        ->write(json_encode(['error' => 'Informe um número de telefone válido.'], JSON_UNESCAPED_UNICODE)); //verificar se o JSON_UNESCAPED_UNICODE é algo válido
+                    
+                    return $response;
+                }
+
+                $phoneExistVerify = $contact->getContactByPhone($data['phone']);
+                
+                if($phoneExistVerify) {
+                    $response
+                    ->withStatus(409)
+                    ->withHeader('Content-Type', 'application/json')
+                    ->getBody()
+                    ->write(json_encode(['message' => 'O phone informado existe em nosso sistema.']));
+
+                    return  $response;
+                }
+            }
+
+            if(isset($data['cellphone'])){
+                if (strlen($data['cellphone']) > 11) {
+                    $response
+                        ->withStatus(400)
+                        ->withHeader('Content-Type', 'application/json')
+                        ->getBody()
+                        ->write(json_encode(['error' => 'Informe um número de celular válido.'], JSON_UNESCAPED_UNICODE));
+                    
+                    return $response;
+                }
+
+                $cellphoneExistVerify = $contact->getContactByCellPhone($data['cellphone']);
+                
+                if($cellphoneExistVerify) {
+                    $response
+                    ->withStatus(409)
+                    ->withHeader('Content-Type', 'application/json')
+                    ->getBody()
+                    ->write(json_encode(['message' => 'O cellphone informado existe em nosso sistema.']));
+
+                    return  $response;
+                }
+            }
 
             $contact->createContact(
                 $data['full_name'],
-                $data['date_of_birth'],
+                $data['birthday'],
                 $data['mail'],
-                $data['profession'],
-                $data['telephone'],
+                $data['occupation'],
+                $data['phone'],
                 $data['cellphone'],
                 $data['have_whatsapp'],
-                $data['send_mail'],
-                $data['send_sms']
+                $data['send_mail_permission'],
+                $data['send_sms_permission']
             );
 
             $response
@@ -36,11 +129,14 @@ final class ContactsControllers  {
             return $response;
 
         } catch (Exception $e) {
-            return $response
+            var_dump($e);
+            $response
                 ->withStatus(500)
                 ->withHeader('Content-Type', 'application/json')
                 ->getBody()
                 ->write(json_encode(['error' => 'Erro interno no servidor']));
+
+            return $response;
         }
     }
 
--- App/models/Contacts.php
+++ App/models/Contacts.php
@@ -11,18 +11,18 @@ class Contacts extends Database {
         
     }
 
-    public function createContact() {
-        $query = $this->pdo->prepare("INSERT INTO contacts(full_name, date_of_birth, mail, profession, telephone, cellphone, have_whatsapp, send_mail, send_sms)VALUES(?, ?, ?, ?, ?, ?, ?, ?,?)");
+    public function createContact($full_name, $birthday, $mail, $occupation, $phone, $cellphone, $have_whatsapp, $send_mail_permission, $send_sms_permission) {
+        $query = $this->pdo->prepare("INSERT INTO contacts(full_name, birthday, mail, occupation, phone, cellphone, have_whatsapp, send_mail_permission, send_sms_permission)VALUES(?, ?, ?, ?, ?, ?, ?, ?,?)");
 
         $query->bindParam(1, $full_name);
-        $query->bindParam(2, $date_of_birth);
+        $query->bindParam(2, $birthday);
         $query->bindParam(3, $mail);
-        $query->bindParam(4, $profession);
-        $query->bindParam(5, $telephone);
+        $query->bindParam(4, $occupation);
+        $query->bindParam(5, $phone);
         $query->bindParam(6, $cellphone);
         $query->bindParam(7, $have_whatsapp);
-        $query->bindParam(8, $send_mail);
-        $query->bindParam(9, $send_sms);
+        $query->bindParam(8, $send_mail_permission);
+        $query->bindParam(9, $send_sms_permission);
 
         $query->execute();
     }
@@ -31,6 +31,32 @@ class Contacts extends Database {
         $result = $this->pdo->query('SELECT * FROM contacts ORDER BY id desc')->fetchAll(\PDO::FETCH_ASSOC);
 
         return $result;
- 
     } 
+
+    public function getContactByMail($mail) {
+        $query = $this->pdo->prepare('SELECT * FROM contacts WHERE mail = ?');
+        $query->execute([$mail]);
+    
+        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+    
+        return $result;
+    }
+
+    public function getContactByPhone($phone) {
+        $query = $this->pdo->prepare('SELECT * FROM contacts WHERE phone = ?');
+        $query->execute([$phone]);
+    
+        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+    
+        return $result;
+    }
+
+    public function getContactByCellPhone($cellphone) {
+        $query = $this->pdo->prepare('SELECT * FROM contacts WHERE cellphone = ?');
+        $query->execute([$cellphone]);
+    
+        $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+    
+        return $result;
+    }
 }
